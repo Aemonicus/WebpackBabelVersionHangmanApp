ed1749691778c5ad860de9af46b2e82720e21a29
Florent VICENTE <vicentegroupe@gmail.com>
2019-11-28 11:37:16 +0100

    fin de projet

---
+++ src/requests.js
@@ -0,0 +1,18 @@
+
+
+// VERSION ASYNC AWAIT
+
+const getPuzzle = async (wordCount) => {
+  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+
+  if (response.status === 200) {
+    const data = await response.json()
+    return data.puzzle
+  } else {
+    throw new Error('Unable to get puzzle')
+  }
+}
+
+
+
+export { getPuzzle as default } 
